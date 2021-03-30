7e1533b95549d0f17646f1036241c532064e9194
sanziowesley <sanziowesley@hotmail.com>
2021-03-29 22:55:45 -0300

    Incluindo comentários em postagens - parte 4

--- api/server.js
+++ api/server.js
@@ -13,6 +13,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(multiparty());
 
+app.use(function(req, res, next) {
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    next();
+
+});
+
 var port = 8080;
 
 app.listen(port);
@@ -31,8 +42,6 @@ app.get('/', function(req, res) {
 
 // POST(create)
 app.post('/api', function(req, res) {
-
-    res.setHeader("Access-Control-Allow-Origin", "*");
 
     var date = new Date();
     var time_stamp = date.getTime();
@@ -71,8 +80,6 @@ app.post('/api', function(req, res) {
 
 // GET(ready)
 app.get('/api', function(req, res) {
-
-    res.setHeader("Access-Control-Allow-Origin", "*");
 
     db.open(function(err, mongoclient) {
         mongoclient.collection('postagens', function(err, collection) {
@@ -128,7 +135,7 @@ app.get('/uploads/:imagem', function(req, res) {
 
 // PUT by ID(update)
 app.put('/api/:id', function(req, res) {
-    res.send('rota para atualizacao');
+    res.send(req.body.comentario);
     /*
     db.open(function(err, mongoclient) {
         mongoclient.collection('postagens', function(err, collection) {
